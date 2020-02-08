484fa57416652fb4243f624d9cc2e56ed5bae77a
Francisco José Plácido da Cunha <fj.placido@gmail.com>
2020-02-08 16:49:30 -0300

    Atualização: utilizando somente JsonIgnore

--- src/main/java/br/com/jsolutions/mc/domain/Cliente.java
+++ src/main/java/br/com/jsolutions/mc/domain/Cliente.java
@@ -15,7 +15,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import br.com.jsolutions.mc.domain.enuns.TipoCliente;
 
@@ -31,7 +30,6 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 
-	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
